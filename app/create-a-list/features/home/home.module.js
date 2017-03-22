1a602740731cb2fd9e04bd3032c068b1ce9650ab
Tara Baker <Tara Baker>
2017-03-22 18:20:48 -0400

    debug dashboard view

--- app/create-a-list/features/home/home.module.js
+++ app/create-a-list/features/home/home.module.js
@@ -1,3 +1,5 @@
 angular
 	//an array is needed every time you are creating a module. If you were connecting to an existing module, you wouldn't need this array
-	.module('createAList.home', []);
+	.module('createAList.home', [
+		'createAList.component.list'
+		]);
